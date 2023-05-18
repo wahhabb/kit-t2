2209b20e3b6cb901a8678540a0b3b4ba55fc59a4
Wahhab Baldwin <wahhabb@gmail.com>
2023-05-18 14:11:06 -0700

    Update svelte.config.js
    Try matching working file from pugblog

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,14 @@
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
-import netlify from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: netlify()
-	}
+		adapter: adapter()
+	},
+	preprocess: preprocess({
+		// ...svelte-preprocess options
+	})
 };
 
 export default config;
